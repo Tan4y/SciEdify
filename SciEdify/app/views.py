3047ae82849e67cf2fc7090db8f1dd43a98771f5
Gabo1234567890 <gabonenov@gmail.com>
2023-05-10 23:07:36 +0300

    homepage.html

--- SciEdify/app/views.py
+++ SciEdify/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def app(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
