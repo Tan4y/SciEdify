d6b18b79d1dcf3bb9bcd09f96021a3140ae83a8e
Tanya <titani4ka@yahoo.com>
2023-05-30 16:13:37 +0300

    style information in biology.hml

--- SciEdify/app/urls.py
+++ SciEdify/app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('app/', views.app, name='app'),
     path('app/biology.html', views.biology, name = 'biology'),
     path('app/chemistry.html', views.chemistry, name='chemisty'),
-    path('app/geography.html', views.chemistry, name='geography'),
-    path('app/mathematics.html', views.chemistry, name='mathematics')
+    path('app/geography.html', views.geography, name='geography'),
+    path('app/mathematics.html', views.mathematics, name='mathematics')
 ]
